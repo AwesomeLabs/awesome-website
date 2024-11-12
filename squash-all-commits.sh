54a5157a0af0d5749eefdfb96cdd4ea5e830b3f8
Bob Troia <btroia@gmail.com>
2024-11-12 11:53:44 -0600

    update product links

--- squash-all-commits.sh
+++
@@ -1,8 +0,0 @@
-git checkout --orphan new-master
-git add .
-git commit -m "Clean commits"
-git branch -m master old-master
-git branch -m new-master master
-git push --force --set-upstream origin master
-git branch -D old-master
-git push
